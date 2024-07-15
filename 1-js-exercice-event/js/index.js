fb013824078ccbefa780623a67513f2178ed2f5e
Faysal Driouach <fdriouach10@gmail.com>
2024-07-15 13:11:57 +0200

    commit 15/07/24

--- 1-js-exercice-event/js/index.js
+++ 1-js-exercice-event/js/index.js
@@ -1,1 +1,12 @@
 console.log("index.js ok");
+
+let btn = document.querySelector(".btn")
+
+btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        lefft: 0,
+        behavior: "smooth"
+    })
+
+})
--- 2-js-exercice-dom/README.md
+++
@@ -1,9 +0,0 @@
-# Créer une page en Javascript
-
-## Fichier de base
-
-Importez le fichier exo-dom.fig dans Figma.
-
-## Consignes
-
-Suivez les consignes indiquées dans la section *consignes*.
